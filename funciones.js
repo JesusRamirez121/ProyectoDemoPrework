755c85b12a70651f0fc2cb5528f778dd152d7b17
JesusRamirez121 <jesus1.ramirez@ucp.edu.co>
2021-12-06 16:18:39 -0500

    tercer commit

---
+++ funciones.js
@@ -0,0 +1,13 @@
+//funcion expresada
+
+let sum = function(NumA,NumB){
+return NumA+NumB;
+}
+console.log(sum(5,45));
+
+//funcion declarada
+
+function rest(NumC,NumD){
+    return NumC-NumD;
+}
+console.log(rest(5,45));
